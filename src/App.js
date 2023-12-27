2d9e57e3501b7b524a551a33f1eadd060367dddb
Sim <0doogadooga@gmail.com>
2023-12-27 21:16:09 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Example from "./example/Example";
+import {useState} from "react";
+
+function App() {
+
+
+    return (
+        <div className="App">
+            <Example/>
+        </div>
+    );
+}
+
+export default App;
